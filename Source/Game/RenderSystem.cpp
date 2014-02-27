12ae219412d1115455fd1eb85dcfea821b2fc046
Peter Doan <doanpiotr@gmail.com>
2014-02-27 09:37:05 +0100

    Use scissor test to limit the game area.

--- Source/Game/RenderSystem.cpp
+++ Source/Game/RenderSystem.cpp
@@ -148,6 +148,14 @@ void RenderSystem::Draw()
     if(!m_initialized)
         return;
 
+    // Clear the depth.
+    Main::CoreRenderer().SetClearDepth(1.0f);
+    Main::CoreRenderer().Clear(ClearFlags::Depth);
+
+    // Clear the screen.
+    Main::CoreRenderer().SetClearColor(glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
+    Main::CoreRenderer().Clear(ClearFlags::Color);
+
     //
     // Setup View
     //
@@ -173,6 +181,7 @@ void RenderSystem::Draw()
     {
         float aspectRatio = windowVerticalAspectRatio / gameVerticalAspectRatio;
 
+        // Scale screen space coordinates.
         screenSpace.x *= aspectRatio;
         screenSpace.y *= aspectRatio;
     }
@@ -181,15 +190,27 @@ void RenderSystem::Draw()
     {
         float aspectRatio = windowHorizontalAspectRatio / gameHorizontalAspectRatio;
 
+        // Scale screen space coordinates.
         screenSpace.z *= aspectRatio;
         screenSpace.w *= aspectRatio;
     }
 
-    // Setup view matrix.
-    glm::mat4 view = glm::ortho(screenSpace.x, screenSpace.y, screenSpace.z, screenSpace.w);
+    // Setup view and projection matrix.
+    glm::mat4 projection = glm::ortho(screenSpace.x, screenSpace.y, screenSpace.z, screenSpace.w);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-gameWidth * 0.5f, -gameHeight * 0.5f, 0.0f));
 
-    // Move origin to bottom left corner.
-    view = glm::translate(view, glm::vec3(-gameWidth * 0.5f, -gameHeight * 0.5f, 0.0f));
+    // Set scissor test area.
+    glm::vec4 viewport(0.0f, 0.0f, windowWidth, windowHeight);
+
+    glm::vec3 position = glm::project(glm::vec3(0.0f, 0.0f, 0.0f), view, projection, viewport);
+    glm::vec3 size = glm::project(glm::vec3(gameWidth, gameHeight, 0.0f), view, projection, viewport) - position;
+
+    glScissor((int)position.x, (int)position.y, (int)size.x, (int)size.y);
+
+    // Toggle scissor test.
+    glEnable(GL_SCISSOR_TEST);
+    
+    SCOPE_GUARD(glDisable(GL_SCISSOR_TEST));
 
     //
     // Clear Screen
@@ -197,8 +218,7 @@ void RenderSystem::Draw()
 
     // Clear the screen.
     Main::CoreRenderer().SetClearColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
-    Main::CoreRenderer().SetClearDepth(1.0f);
-    Main::CoreRenderer().Clear(ClearFlags::Color | ClearFlags::Depth);
+    Main::CoreRenderer().Clear(ClearFlags::Color);
 
     //
     // Draw Sprites
@@ -215,7 +235,7 @@ void RenderSystem::Draw()
     glUseProgram(m_shader.GetHandle());
     glBindVertexArray(m_vertexInput.GetHandle());
 
-    glUniformMatrix4fv(m_shader.GetUniform("viewTransform"), 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(m_shader.GetUniform("viewTransform"), 1, GL_FALSE, glm::value_ptr(projection * view));
     glUniform1i(m_shader.GetUniform("texture"), 0);
 
     // Batch sprites.
