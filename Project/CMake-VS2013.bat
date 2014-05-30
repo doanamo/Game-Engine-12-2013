391aa892e79798214d69f069fdcee75f3f00ed96
Peter Doan <doanpiotr@gmail.com>
2014-05-30 17:32:23 +0200

    Cleanup build system configuration.

--- Project/CMake-VS2013.bat
+++ Project/CMake-VS2013.bat
@@ -1,10 +1,13 @@
 @echo off
 
+REM Create an empty output directory.
 cd ..
 rd /s /q Build
 mkdir Build
 cd Build
 
+REM Generate a native project solution.
 cmake -g "Visual Studio 12" ../Project
 
+REM Prevent from automatically closing the console window.
 pause
