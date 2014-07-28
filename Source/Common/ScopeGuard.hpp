56e6b1f078bda7e5c914f2c1d3a1332956960367
Peter Doan <doanpiotr@gmail.com>
2014-07-28 12:25:13 +0200

    Add new useful scope guard macros and improve documentation.

--- Source/Common/ScopeGuard.hpp
+++ Source/Common/ScopeGuard.hpp
@@ -13,9 +13,29 @@
 //          delete[] array;
 //      });
 //
-//  Using a cope guard macro:
+//  Using a scope guard macro:
 //      int* array = new int[10];
 //      SCOPE_GUARD(delete[] array);
+//
+//  Using a conditional scope guard macro:
+//      bool cleanup = true;
+//      int* array = new int[10];
+//      SCOPE_GUARD_IF(cleanup, delete[] array);
+//
+//  Using braced scope guard macros:
+//      int* array = new int[10];
+//      SCOPE_GUARD_BEGIN()
+//          delete[] array;
+//          array = nullptr;
+//      SCOPE_GUARD_END()
+//
+//  Using braced conditonal scope guard macros:
+//      bool cleanup = true;
+//      int* array = new int[10];
+//      SCOPE_GUARD_BEGIN(cleanup)
+//          delete[] array;
+//          array = nullptr;
+//      SCOPE_GUARD_END()
 //
 
 // Scope guard class.
@@ -64,8 +84,38 @@ ScopeGuard<Type> MakeScopeGuard(Type function)
     return ScopeGuard<Type>(function);
 }
 
+// Scope guard condition.
+class ScopeGuardCondition
+{
+public:
+    ScopeGuardCondition()
+    {
+        m_condition = true;
+    }
+
+    ScopeGuardCondition(bool condition)
+    {
+        m_condition = condition;
+    }
+
+    explicit operator bool()
+    {
+        return m_condition;
+    }
+
+private:
+    bool m_condition;
+};
+
 // Utility macros.
+#define SCOPE_GUARD_BEGIN(...) auto SCOPE_GUARD_NAME(__LINE__) = MakeScopeGuard([&]() { if(ScopeGuardCondition(__VA_ARGS__)) { 
+#define SCOPE_GUARD_END() } });
+
 #define SCOPE_GUARD(code) auto SCOPE_GUARD_NAME(__LINE__) = SCOPE_GUARD_MAKE(code)
 #define SCOPE_GUARD_MAKE(code) MakeScopeGuard([&]() { code; })
+
+#define SCOPE_GUARD_IF(condition, code) auto SCOPE_GUARD_NAME(__LINE__) = SCOPE_GUARD_IF_MAKE(condition, code)
+#define SCOPE_GUARD_IF_MAKE(condition, code) MakeScopeGuard([&]() { if(condition) { code; } })
+
 #define SCOPE_GUARD_NAME(line) SCOPE_GUARD_STRING(line)
 #define SCOPE_GUARD_STRING(line) scopeGuard_Line ## line
