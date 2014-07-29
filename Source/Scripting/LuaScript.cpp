a56d0ce8343c837b8c56219f8bc5000949d30d09
Peter Doan <doanpiotr@gmail.com>
2014-07-29 03:00:41 +0200

    Implement variable retrieving from a lua script.

--- Source/Scripting/LuaScript.cpp
+++ Source/Scripting/LuaScript.cpp
@@ -8,7 +8,8 @@ namespace
 }
 
 LuaScript::LuaScript() :
-    m_state(nullptr)
+    m_state(nullptr),
+    m_level(0)
 {
 }
 
@@ -53,6 +54,153 @@ void LuaScript::Cleanup()
         lua_close(m_state);
         m_state = nullptr;
     }
+
+    m_level = 0;
+}
+
+std::string LuaScript::DetachStem(std::string& compound)
+{
+    // Find the separator.
+    std::size_t separator = compound.find_first_of('.');
+
+    // Copy the stem.
+    std::string detached(compound, 0, separator);
+
+    // Erase detached stem.
+    if(separator != std::string::npos)
+    {
+        compound.erase(0, separator + 1);
+    }
+    else
+    {
+        compound.erase();
+    }
+
+    return detached;
+}
+
+bool LuaScript::PushVariable(std::string compound)
+{
+    assert(m_state);
+
+    // Push compound on the stack.
+    std::string element;
+
+    while(true)
+    {
+        // Detach element name from variable compound.
+        element = DetachStem(compound);
+
+        if(element.empty())
+            break;
+
+        // Push variable on the stack.
+        if(m_level == 0)
+        {
+            lua_getglobal(m_state, element.c_str());
+        }
+        else
+        {
+            lua_getfield(m_state, -1, element.c_str());
+        }
+
+        m_level++;
+
+        // Check if we pushed nil.
+        if(lua_isnil(m_state, -1))
+            return false;
+    }
+
+    return true;
+}
+
+void LuaScript::PopStack()
+{
+    assert(m_state);
+
+    // Pop the entire stack.
+    lua_pop(m_state, m_level);
+    m_level = 0;
+}
+
+std::string LuaScript::GetString(std::string compound, std::string unavailable)
+{
+    std::string value = unavailable;
+
+    if(m_state)
+    {
+        if(PushVariable(compound))
+        {
+            if(lua_isstring(m_state, -1))
+            {
+                value = lua_tostring(m_state, -1);
+            }
+            
+            PopStack();
+        }
+    }
+
+    return value;
+}
+
+bool LuaScript::GetBool(std::string compound, bool unavailable)
+{
+    bool value = unavailable;
+
+    if(m_state)
+    {
+        if(PushVariable(compound))
+        {
+            if(lua_isboolean(m_state, -1))
+            {
+                value = lua_toboolean(m_state, -1) != 0;
+            }
+            
+            PopStack();
+        }
+    }
+
+    return value;
+}
+
+int LuaScript::GetInteger(std::string compound, int unavailable)
+{
+    int value = unavailable;
+
+    if(m_state)
+    {
+        if(PushVariable(compound))
+        {
+            if(lua_isnumber(m_state, -1))
+            {
+                value = (int)lua_tonumber(m_state, -1);
+            }
+            
+            PopStack();
+        }
+    }
+
+    return value;
+}
+
+float LuaScript::GetFloat(std::string compound, float unavailable)
+{
+    float value = unavailable;
+
+    if(m_state)
+    {
+        if(PushVariable(compound))
+        {
+            if(lua_isnumber(m_state, -1))
+            {
+                value = (float)lua_tonumber(m_state, -1);
+            }
+            
+            PopStack();
+        }
+    }
+
+    return value;
 }
 
 bool LuaScript::IsValid() const
--- Source/Scripting/LuaScript.hpp
+++ Source/Scripting/LuaScript.hpp
@@ -15,8 +15,19 @@ public:
     bool Load(std::string filename);
     void Cleanup();
 
+    std::string GetString(std::string compound, std::string unavailable = "nil");
+    bool GetBool(std::string compound, bool unavailable = false);
+    int GetInteger(std::string compound, int unavailable = 0);
+    float GetFloat(std::string compound, float unavailable = 0.0f);
+
     bool IsValid() const;
 
 private:
+    std::string DetachStem(std::string& compound);
+    bool PushVariable(std::string compound);
+    void PopStack();
+
+private:
     lua_State* m_state;
+    int m_level;
 };
